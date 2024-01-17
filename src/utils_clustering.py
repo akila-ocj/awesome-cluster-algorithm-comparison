160a61ebf9018a819cd99a43985dd1cbea2a521e
akila <akilaocj@gmail.com>
2024-01-17 14:02:27 +1100

    Add spectral biclustering

--- src/utils_clustering.py
+++ src/utils_clustering.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from sklearn import metrics
 import pandas as pd
+import numpy as np
 from datetime import datetime
 
 def plot_clusters(data, labels_pred, title='Clustering Visualization'):
@@ -77,3 +78,48 @@ def load_labels_from_file(file_path, labels_pred_len):
                          f"definition.")
 
     return labels_true
+
+
+# Function to visualize the biclusters
+def visualize_biclusters(data, row_labels, col_labels, title='Biclustering Visualization'):
+    sorted_data = data[np.argsort(row_labels), :]
+    sorted_data = sorted_data[:, np.argsort(col_labels)]
+
+    plt.figure(figsize=(12, 8))  # Increase figure size
+    plt.imshow(sorted_data, aspect='auto', cmap='viridis')
+    plt.colorbar()  # Add a color bar
+    plt.grid(False)  # Disable grid (set to True to enable)
+    plt.title(title)
+    plt.xlabel('Columns')
+    plt.ylabel('Rows')
+    plt.show()
+
+
+def evaluate_biclustering(data,  labels_true, bicluster, clustering_name, dataset_name,results_path):
+    """
+    Evaluates the biclustering performance using various metrics.
+
+    :param data: Feature set.
+    :param labels_true: Ground truth labels for traditional clustering metrics.
+    :param bicluster: Fitted biclustering object.
+    :param clustering_name: Name of the clustering algorithm.
+    :param dataset_name: Name of the dataset.
+    :param results_path: Path to save the results CSV file.
+    """
+    # Collecting metrics
+    results = {
+        'Timestamp': datetime.now(),
+        'Dataset': dataset_name,
+        'Clustering Algorithm': clustering_name,
+        'AMI': metrics.adjusted_mutual_info_score(labels_true, bicluster.row_labels_),
+        'Silhouette Score': metrics.silhouette_score(data, bicluster.row_labels_),
+        # 'Consensus Score': metrics.consensus_score(bicluster.biclusters_, bicluster.biclusters_),
+    }
+
+    # Print results
+    for key, value in results.items():
+        print(f"{key}: {value}")
+
+    # Save to CSV
+    df = pd.DataFrame([results])
+    df.to_csv(results_path, mode='a', header=not pd.io.common.file_exists(results_path), index=False)
